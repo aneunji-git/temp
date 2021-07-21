34e410d929b989c4e58aaf5f7e4c79637cb35b3e
eunji <dmswl7948@gmail.com>
2021-07-21 12:54:05 +0900

    7/21 2ban class commit 4

--- decorators.py
+++ decorators.py
@@ -8,6 +8,29 @@ def decorator(func):
 @decorator
 def hello_world(input_text):
     print(input_text)
-    
+
 
 hello_world("Hello World")
+
+
+
+def decorator_1(func):
+    def decorated_1(base, height):
+        if base<0 or height<0:
+            print('Error')
+        else:
+            func(base, height)
+    return decorated_1
+
+@decorator_1
+def triangle(base, height):
+    print(base * height* 0.5)
+
+@decorator_1
+def rectangle(base, height):
+    print(base * height)
+
+
+
+tri1 = triangle(4,7)
+print(tri1)
