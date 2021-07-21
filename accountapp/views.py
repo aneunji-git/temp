eb143588f7c6d5ecf785f60ace310706cbab32d1
eunji <dmswl7948@gmail.com>
2021-07-21 12:06:40 +0900

    7/21 2ban class commit 3

--- accountapp/views.py
+++ accountapp/views.py
@@ -28,10 +28,12 @@ class AccountCreateView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/create.html'
+
 class AccountDetailView(DetailView):
     model = User
     context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
+    
 class AccountUpdateView(UpdateView):
     model = User
     form_class = AccountCreationForm
