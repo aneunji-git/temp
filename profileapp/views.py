9fb2e7ef0d804cfab7ac8473a1d8ecdab2b9a499
eunji <dmswl7948@gmail.com>
2021-07-26 19:44:26 +0900

    7/26 2ban class commit 3

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-from django.shortcuts import render
-
-# Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -17,3 +13,7 @@ class ProfileCreateView(CreateView):
     form_class = ProfileCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
